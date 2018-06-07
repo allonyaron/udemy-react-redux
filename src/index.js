3a88edd98f89f80f40b1969c13a111e17d89fedb
Allon Yaron <allonyaron@gmail.com>
2018-06-07 09:37:56 -0400

    refactor App to class component

--- src/index.js
+++ src/index.js
@@ -8,12 +8,20 @@ import SearchBar from './components/search_bar';
 const API_KEY = 'AIzaSyAb7EHP4pa1t-eJE5FjquCGmiZx0Y3x82Y';
 
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function (data) {
-	console.log(data);
-});
+
 
 
 class App extends Component {
+	constructor(props) {
+		super(props);
+		
+		this.state = { videos: [] };
+
+		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+			this.setState({ videos });
+		});
+
+	}
 	render() {
 		return (
 			<div>
