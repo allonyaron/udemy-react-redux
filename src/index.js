525aa7c7c679948fd329cbd12e3b9994427907c8
Allon Yaron <allonyaron@gmail.com>
2018-06-07 09:37:19 -0400

    refactor App to class component

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
@@ -10,17 +10,17 @@ const API_KEY = 'AIzaSyAb7EHP4pa1t-eJE5FjquCGmiZx0Y3x82Y';
 
 YTSearch({key: API_KEY, term: 'surfboards'}, function (data) {
 	console.log(data);
-})
+});
 
 
-const App = () => {
-	// For multiline jsx it is usually wrapped in prentheses - 
-	// otherwise first div needs to be on return line or it will error
-	return (
-		<div>
-			<SearchBar />
-		</div>
-	);
+class App extends Component {
+	render() {
+		return (
+			<div>
+				<SearchBar />
+			</div>
+		);
+	}
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
