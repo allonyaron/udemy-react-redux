9c5a173f839a277769fd49c5ce3450a987a4c93c
Allon Yaron <allonyaron@gmail.com>
2018-06-07 13:27:39 -0400

    added click handler for video select

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 // TODO - Move api key to safe place to distribute
 const API_KEY = 'AIzaSyAb7EHP4pa1t-eJE5FjquCGmiZx0Y3x82Y';
@@ -15,10 +17,17 @@ class App extends Component {
 	constructor(props) {
 		super(props);
 		
-		this.state = { videos: [] };
+		this.state = { 
+			videos: [],
+			selectedVideo: null
+		};
 
 		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-			this.setState({ videos });
+			// add default selected video
+			this.setState({ 
+				videos:videos, 
+				selectedVideo: videos[0]
+			});
 		});
 
 	}
@@ -26,6 +35,10 @@ class App extends Component {
 		return (
 			<div>
 				<SearchBar />
+				<VideoDetail video={this.state.selectedVideo} />
+				<VideoList 
+				onVideoSelect={(selectedVideo) => this.setState({selectedVideo}) }
+				videos={this.state.videos} />
 			</div>
 		);
 	}
