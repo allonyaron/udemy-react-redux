b036426e4d0c333b83360a84efc0d1916dcb6824
Allon Yaron <allonyaron@gmail.com>
2018-06-10 15:42:12 -0400

    added pages to book reducer

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
 	return [
-		{ title: 'Javascript: The Good Parts'},
-		{ title: 'Confederacy of Dunces'},
-		{ title: 'Into the Wild'},
-		{ title: 'Eloquent Javascript'}
+		{ title: 'Javascript: The Good Parts', pages: 101},
+		{ title: 'Confederacy of Dunces', pages: 40},
+		{ title: 'Into the Wild', pages: 30},
+		{ title: 'Eloquent Javascript', pages: 234}
 	]
 }
