f3da5dc2fc80400b45ad7f798ff1d7c16010d162
Allon Yaron <allonyaron@gmail.com>
2018-06-10 14:48:52 -0400

    wired up book detail reducer

--- src/containers/book_detail.js
+++ src/containers/book_detail.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class BookDetail extends Component {
+class BookDetail extends Component {
 	render() {
+		// FOR INITIAL LOAD WHEN state is null
+		if(!this.props.book) {
+			return <div>Please Select a Book</div>;
+		}
+
 		return (
-				<div>Book Detail</div>
+				<div>
+					<h3>Details for:</h3>
+					<div>{this.props.book.title}</div>
+				</div>
 			);
 	}
 }
+
+function mapStateToProps(state) {
+	return {
+		book: state.activeBook
+	};
+}
+
+export default connect(mapStateToProps)(BookDetail);
