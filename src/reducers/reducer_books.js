442fd867980b1762ae91f065930742b153f9ed34
Allon Yaron <allonyaron@gmail.com>
2018-06-10 14:36:21 -0400

    built out action Creators; stubbed out book detail

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+	return [
+		{ title: 'Javascript: The Good Parts'},
+		{ title: 'Confederacy of Dunces'},
+		{ title: 'Into the Wild'},
+		{ title: 'Eloquent Javascript'}
+	]
+}
